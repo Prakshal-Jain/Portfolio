edfa1eaf0c4458ced842af1e0e88c2ca5a4fa060
Prakshal Jain <prakshaljain@meta.com>
2023-06-13 11:17:15 -0700

    updated experiences

--- src/data/experience_list.tsx
+++ src/data/experience_list.tsx
@@ -14,17 +14,10 @@ export const experiences = [
         description: (
             <ul>
                 <li>
-                    Gained 91% of the mobile-based customers of the total website visitors by developing and integrating
-                    embedded signup flow for the mobile site with the back end application.
+                    Facebook Stories and Reels.
                 </li>
                 <li>
-                    Accelerated the company-wide mobile web development process time by 70% by introducing an
-                    internal mobile React component library. This boosted the product launch timeline from 7 months to only
-                    3.5 months, amplified UI and code consistency, end to end Jest testing, and served code reduction.
-                </li>
-                <li>
-                    Worked closely with UX designers and project managers to refine existing UI, documented suggestions, worked on bug fixes, created hooks for GraphQL API calls, added Flow type checking, and reorganized
-                    the existing codebase for a more efficient development process beyond the scope of my intern project.
+                    Skills: MySQL · Git · WebSocket · Jest · React Native · REST APIs · JSON · AJAX · TypeScript · LESS (Stylesheet Language) · SASS · HTML5 · Flow Type Testing · GraphQL · PHP · Python (Programming Language) · JavaScript · User Experience (UX) · jQuery · Cascading Style Sheets (CSS) · Keynote · Leadership · Teamwork · User Interface Programming · DHTML · User Interface Prototyping · React.js
                 </li>
             </ul>
         ),
